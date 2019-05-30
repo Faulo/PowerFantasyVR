82cea731381ef43baa6d87264a20341b141b7d0e
Daniel Schulz <faulolio@gmail.com>
2019-05-30 15:35:35 +0200

    hello ML.NET

---
+++ Assets/Scripts/GameBehaviour.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.ML;
+using ShaderGraphML.Model.DataModels;
+using System.Diagnostics;
+using System.IO;
+
+public class GameBehaviour : MonoBehaviour
+{
+    private const string MODEL_FILEPATH = @"MLModel.zip";
+    public static string GetAbsolutePath(string relativePath) {
+        FileInfo _dataRoot = new FileInfo(typeof(GameBehaviour).Assembly.Location);
+        string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+        string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+        UnityEngine.Debug.Log(fullPath);
+        return fullPath;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //*
+        MLContext mlContext = new MLContext();
+
+        // Training code used by ML.NET CLI and AutoML to generate the model
+        //ModelBuilder.CreateModel();
+
+        ITransformer mlModel = mlContext.Model.Load(GetAbsolutePath(MODEL_FILEPATH), out DataViewSchema inputSchema);
+        var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+        
+        // Create sample data to do a single prediction with it 
+        ModelInput sampleData = new ModelInput {
+            Kleiner = 0.9f,
+            Ring = 0.9f,
+            Mittel = 0.1f,
+            Zeige = 0.9f,
+            Daumen = 0.9f,
+            Geste = "Mittelfinger"
+        };
+
+        // Try a single prediction
+        Stopwatch sw = new Stopwatch();
+
+        sw.Start();
+        ModelOutput predictionResult = predEngine.Predict(sampleData);
+        sw.Stop();
+
+        UnityEngine.Debug.Log(predictionResult.Prediction);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
