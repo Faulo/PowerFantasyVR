d8a2bd40e2234b53a67fcd2069a2b3341d4c5790
Daniel Schulz <faulolio@gmail.com>
2019-05-30 17:48:42 +0200

    namespacing and basic gesture recognition!

--- Assets/Scripts/GameBehaviour.cs
+++ Assets/Scripts/GameBehaviour.cs
@@ -5,9 +5,11 @@ using Microsoft.ML;
 using PFVR.DataModels;
 using System.Diagnostics;
 using System.IO;
+using PFVR.Tracking;
 
 public class GameBehaviour : MonoBehaviour
 {
+    private PredictionEngine<OneHand, StringPrediction> engine;
     private const string MODEL_FILEPATH = @"/Models/manus.zip";
     public static string GetAbsolutePath(string relativePath) {
         return Application.dataPath + relativePath;
@@ -16,39 +18,44 @@ public class GameBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*
         MLContext mlContext = new MLContext();
 
         // Training code used by ML.NET CLI and AutoML to generate the model
         //ModelBuilder.CreateModel();
 
         ITransformer mlModel = mlContext.Model.Load(GetAbsolutePath(MODEL_FILEPATH), out DataViewSchema inputSchema);
-        var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+        engine = mlContext.Model.CreatePredictionEngine<OneHand, StringPrediction>(mlModel);
         
         // Create sample data to do a single prediction with it 
-        ModelInput sampleData = new ModelInput {
+        var sampleData = new OneHand {
             Kleiner = 0.9f,
             Ring = 0.9f,
             Mittel = 0.1f,
             Zeige = 0.9f,
             Daumen = 0.9f,
-            Geste = "Mittelfinger"
+            Geste = ""
         };
 
         // Try a single prediction
         Stopwatch sw = new Stopwatch();
 
         sw.Start();
-        ModelOutput predictionResult = predEngine.Predict(sampleData);
+        var predictionResult = engine.Predict(sampleData);
         sw.Stop();
 
         UnityEngine.Debug.Log(predictionResult.Prediction);
-        //*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (engine != null) {
+            var hand = ManusConnector.leftHand;
+            UnityEngine.Debug.Log(hand);
+            if (hand != null) {
+                var predictionResult = engine.Predict(hand);
+                UnityEngine.Debug.Log(predictionResult.Prediction);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Tracking/ManusConnector.cs
@@ -0,0 +1,32 @@
+﻿using ManusVR.Core.Apollo;
+using ManusVR.Core.Hands;
+using PFVR.DataModels;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PFVR.Tracking {
+
+    public static class ManusConnector {
+        private static int playerId = 0;
+        public static OneHand leftHand {
+            get {
+                if (playerId == 0) {
+                    HandDataManager.GetPlayerNumber(out playerId);
+                }
+                    if (HandDataManager.CanGetHandData(playerId, device_type_t.GLOVE_LEFT)) {
+                        ApolloHandData apolloHandData = HandDataManager.GetHandData(playerId, device_type_t.GLOVE_LEFT);
+                        return new OneHand {
+                            Kleiner = (float) apolloHandData.fingers[(int)ApolloHandData.FingerName.Pinky].flexSensorRaw[(int)ApolloHandData.FlexSensorSegment.Medial],
+                            Ring = (float) apolloHandData.fingers[(int)ApolloHandData.FingerName.Ring].flexSensorRaw[(int)ApolloHandData.FlexSensorSegment.Medial],
+                            Mittel = (float) apolloHandData.fingers[(int)ApolloHandData.FingerName.Middle].flexSensorRaw[(int)ApolloHandData.FlexSensorSegment.Medial],
+                            Zeige = (float) apolloHandData.fingers[(int)ApolloHandData.FingerName.Index].flexSensorRaw[(int)ApolloHandData.FlexSensorSegment.Medial],
+                            Daumen = (float) apolloHandData.fingers[(int)ApolloHandData.FingerName.Thumb].flexSensorRaw[(int)ApolloHandData.FlexSensorSegment.Medial]
+                        };
+                    }
+                return null;
+            }
+        }
+    }
+
+}
