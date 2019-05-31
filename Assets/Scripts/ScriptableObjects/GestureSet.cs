7bbc57d7fb161c205abc75fc8f6d57a1b5ea350d
Daniel Schulz <faulolio@gmail.com>
2019-05-31 10:57:10 +0200

    shader stuff for Magnus

---
+++ Assets/Scripts/ScriptableObjects/GestureSet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PFVR.ScriptableObjects {
+    [CreateAssetMenu(fileName = "New Gesture Set", menuName = "Gameplay/Gesture Set", order = 1)]
+    public class GestureSet : ScriptableObject {
+        [SerializeField]
+        private Gesture[] gestures;
+    }
+}
--- Assets/Scripts/Tracking/Gesture.cs
+++ Assets/Scripts/Tracking/Gesture.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gesture : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+namespace PFVR.ScriptableObjects {
+    [Serializable]
+    public class Gesture {
+        public string id;
+        public Material material;
     }
 }
