76c74fcc962c3ac3b7877ff5f6162e2ea6659af1
Katharina Broswik <katharina_B@live.de>
2019-06-05 18:05:30 +0200

    Simple A Star behavior
    Implemented simple A Star behavior with Unity Plugin

---
+++ Assets/Scripts/AI/EnemyBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+namespace PFVR.AI
+{
+    public class EnemyBehavior : MonoBehaviour
+    {
+        public GameObject player;
+        public Transform targetPosition;
+        public Vector3 playerPos;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Seeker seeker = GetComponent<Seeker>();
+            seeker.StartPath(transform.position, targetPosition.position, OnPathComplete);
+            playerPos = player.transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(playerPos * Time.deltaTime * 0.2f);
+
+        }
+
+        public void OnPathComplete(Path p)
+        {
+            Debug.Log("Created Path" + p.error);
+        }
+    }
+}
