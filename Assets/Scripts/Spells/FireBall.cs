e09f86350fa2cd673b19a83ff969d2766964f847
Daniel Schulz <faulolio@gmail.com>
2019-06-10 14:33:50 +0200

    fireball scripts+prefabs

---
+++ Assets/Scripts/Spells/FireBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PFVR.Player;
+using UnityEngine;
+
+namespace PFVR.Spells {
+    public class FireBall : MonoBehaviour, ISpellState {
+        [SerializeField]
+        private GameObject ballPrefab;
+
+        public void OnEnter(PlayerBehaviour player, PlayerHandBehaviour hand) {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnExit(PlayerBehaviour player, PlayerHandBehaviour hand) {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnUpdate(PlayerBehaviour player, PlayerHandBehaviour hand) {
+            throw new System.NotImplementedException();
+        }
+        void Start() {
+
+        }
+    }
+
+}
