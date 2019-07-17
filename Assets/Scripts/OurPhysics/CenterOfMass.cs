8521db5c28aef6377a41086447169193bd71fdc0
Daniel Schulz <faulolio@gmail.com>
2019-07-17 16:38:53 +0200

    we all fall down

--- Assets/Scripts/OurPhysics/CenterOfMass.cs
+++ Assets/Scripts/OurPhysics/CenterOfMass.cs
@@ -6,12 +6,9 @@ namespace PFVR.OurPhysics {
     public class CenterOfMass : MonoBehaviour {
         [SerializeField]
         private Rigidbody targetRigidbody = default;
-        [SerializeField, Range(0, 100)]
-        private float density = 1;
-        void Awake() {
+        void Start() {
             if (targetRigidbody) {
                 targetRigidbody.centerOfMass = transform.position - targetRigidbody.transform.position - transform.up;
-                targetRigidbody.mass = density * targetRigidbody.transform.localScale.magnitude;
             }
         }
     }
