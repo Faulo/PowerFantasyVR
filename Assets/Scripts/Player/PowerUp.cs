7b058ab242a3bb66cf09a8396dea0b6d9fa71fd8
Daniel Schulz <faulolio@gmail.com>
2019-07-17 23:41:41 +0200

    namespaces

--- Assets/Scripts/Player/PowerUp.cs
+++ Assets/Scripts/Player/PowerUp.cs
@@ -3,29 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using PFVR.ScriptableObjects;
 
-public class PowerUp : MonoBehaviour
-{
-    [SerializeField]
-    private Gesture gestureToUnlock = default;
+namespace PFVR.Player {
+    public class PowerUp : MonoBehaviour {
+        [SerializeField]
+        private Gesture gestureToUnlock = default;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        void OnTriggerEnter(Collider other) {
+            if (other.gameObject.layer == LayerMask.GetMask("Player")) {
+                Debug.Log("Neue Fähigkeit verfügbar!");
+            }
+        }
+
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }    
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.layer == LayerMask.GetMask("Player"))
-        {
-            Debug.Log("Neue Fähigkeit verfügbar!");
-        }        
-    }
-    
 }
