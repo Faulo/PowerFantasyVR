962c8b0cadbbd08131fb4cafd6c65b9371777a0b
Daniel Schulz <faulolio@gmail.com>
2019-07-17 16:01:39 +0200

    roly poly trees!

---
+++ Assets/Scripts/OurPhysics/CenterOfMass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PFVR.OurPhysics {
+    public class CenterOfMass : MonoBehaviour {
+        [SerializeField]
+        private Rigidbody targetRigidbody = default;
+        [SerializeField, Range(0, 100)]
+        private float density = 1;
+        void Awake() {
+            if (targetRigidbody) {
+                targetRigidbody.centerOfMass = transform.position - targetRigidbody.transform.position - transform.up;
+                targetRigidbody.mass = density * targetRigidbody.transform.localScale.magnitude;
+            }
+        }
+    }
+}
