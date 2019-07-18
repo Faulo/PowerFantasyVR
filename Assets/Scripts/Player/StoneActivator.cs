16770e3c141ea1ec712801bfaecb36c8fb065bc7
Katharina Broswik <Katharina_B@live.de>
2019-07-18 02:13:38 +0200

    Activate Stones

--- Assets/Scripts/Player/StoneActivator.cs
+++ Assets/Scripts/Player/StoneActivator.cs
@@ -1,4 +1,6 @@
-﻿using PFVR.ScriptableObjects;
+﻿using PFVR.Player;
+using PFVR.ScriptableObjects;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +8,20 @@ using UnityEngine;
 public class StoneActivator : MonoBehaviour
 {
     public Gesture gesture;
+    public GameObject stone;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GestureConnector.onGestureUnlock += ActivateStone;
+    }
+
+    private void ActivateStone(Gesture gesture)
+    {
+        if(this.gesture.Equals(gesture))
+        {
+            stone.SetActive(true);
+        }
     }
 
     // Update is called once per frame
