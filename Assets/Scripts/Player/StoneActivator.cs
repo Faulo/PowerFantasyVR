a57fee4b9bee7bac731ee4e1403ac2c1276c220d
Daniel Schulz <faulolio@gmail.com>
2019-07-18 02:42:12 +0200

    optimized stone activator

--- Assets/Scripts/Player/StoneActivator.cs
+++ Assets/Scripts/Player/StoneActivator.cs
@@ -5,28 +5,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoneActivator : MonoBehaviour
-{
+public class StoneActivator : MonoBehaviour {
     public Gesture gesture;
     public GameObject stone;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         GestureConnector.onGestureUnlock += ActivateStone;
+        GestureConnector.onGestureLock += DeactivateStone;
     }
 
-    private void ActivateStone(Gesture gesture)
-    {
-        if(this.gesture.Equals(gesture))
-        {
+    private void ActivateStone(Gesture gesture) {
+        if (this.gesture.Equals(gesture)) {
             stone.SetActive(true);
+            stone.GetComponentInChildren<MeshRenderer>().material.SetColor("_EmissionColor", gesture.spellColor);
+        }
+    }
+    private void DeactivateStone(Gesture gesture) {
+        if (this.gesture.Equals(gesture)) {
+            stone.SetActive(false);
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
