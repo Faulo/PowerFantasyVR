e46f00c3f1ea4b83211183e11eaa990bf38f0d7e
Katharina Broswik <Katharina_B@live.de>
2019-08-06 21:25:24 +0200

    Two kinds of enemies
    Created two kinds of enemies to set in the enemy spawner: enemies with a rigidbody, moved by AddForce (Swarm Enemy (Rigid)) and enemies without a rigidbody moved with translate (Swarm Enemy)

--- Assets/Scripts/AI/EnemyBehaviorTranslate.cs
+++ Assets/Scripts/AI/EnemyBehaviorTranslate.cs
@@ -9,14 +9,14 @@ using PFVR.Player;
 namespace PFVR.AI
 {
     // Additional behavior for enemies (besides homing in on a target)
-    public class EnemyBehavior : MonoBehaviour
+    public class EnemyBehaviorTranslate : MonoBehaviour
     {
+        // Fields to set
         // Alpha > 0; Factor for velocity of agents
-        [SerializeField, Range(0, 10)]
+        [SerializeField, Range(0, 5)]
         private float alphaFactor = 0.4f;
-        private float alphaFactorUsed;
-        // Beta > 0; Factor for diffusion intensity
 
+        // Beta > 0; Factor for diffusion intensity
         [SerializeField, Range(0, 10)]
         private float betaFactor = 1.0f;
 
@@ -24,35 +24,34 @@ namespace PFVR.AI
         private float playerVelocityTheshold = 100.0f;
 
         [SerializeField, Range(0, 1000)]
+        private float playerDistanceThreshold = 30.0f;
+
+        [SerializeField, Range(0, 1000)]
         private float idleDiffusion = 100.0f;
+
+        [SerializeField, Range(0, 1000)]
         private float chasingDiffusion = 0.0f;
 
-        private float diffusion;
-        private float goalDiffusion;
-        private float randNum;
-        private float diffusionX;
-        private float diffusionY;
-        private float diffusionZ;
-        
+        // Related scripts
         private LeaderBehavior leaderBehavior;
-        private Transform nearestGoal;
 
+        // Transformation calculations
+        private float alphaFactorUsed;
+        private float randNum;
+        private Transform nearestGoal;
         private Vector3 diffusionVector;
         private Vector3 finalMovementVector;
-        private Vector3 finalGoalMovementVector;
         private Vector3 transformationVector;
         private Vector3 randomVector;
-        private Vector3 evadeVector;
-        private float playerVelocity;
 
-
+        // Game Objects for pathing
         private GameObject[] arrayOfBeacons;
-        private GameObject leaders;
-        private PlayerBehaviour playerBehavior;
+        
+        // Evation variables
         private IMotor playerMotor;
         private bool evadePlayer;
-
-        private int evadeDirection;
+        private float playerVelocity;
+        private Vector3 evadeVector;
         private Func<Vector3, Vector3, Vector3> evadeBehavior;
 
 
@@ -61,18 +60,12 @@ namespace PFVR.AI
         {
             // Retrieve all Beacons
             arrayOfBeacons = GameObject.FindGameObjectsWithTag("Beacon");
-            leaders = GameObject.FindGameObjectWithTag("Leaders");
-
-            leaderBehavior = leaders.GetComponent<LeaderBehavior>();
-            nearestGoal = arrayOfBeacons[0].transform;
-
-            playerBehavior = leaderBehavior.GetPlayer();
-            //playerRigidbody = playerBehavior.GetComponent<Rigidbody>();
-            playerMotor = playerBehavior.motor;
+            leaderBehavior = GameObject.FindGameObjectWithTag("Leaders").GetComponent<LeaderBehavior>();
+            
+            //playerMotor = leaderBehavior.GetPlayer().motor;
 
             evadeVector = new Vector3();
-            evadeDirection = UnityEngine.Random.Range(0, 3);
-            evadeBehavior = EnemyEvation.FindEvadeBehavior(evadeDirection);
+            evadeBehavior = EnemyEvation.FindEvadeBehavior(UnityEngine.Random.Range(0, 3));
 
             StartCoroutine(FindGoalRoutine());
             //StartCoroutine(EvadePlayerRoutine());
@@ -101,18 +94,20 @@ namespace PFVR.AI
                 yield return wait;
             }
         }
-
+        
         IEnumerator EvadePlayerRoutine()
         {
             var wait = new WaitForSeconds(0.1f);
             while (true)
             {
                 playerVelocity = playerMotor.velocity.magnitude;
-                if (playerVelocity > playerVelocityTheshold && Vector3.Distance(transform.position, playerMotor.position) < 30.0f)
+                // Evade the player when they have at least a minimum speed AND only when near
+                if (playerVelocity > playerVelocityTheshold && Vector3.Distance(transform.position, playerMotor.position) < playerDistanceThreshold)
                 {
                     evadePlayer = true;
                     Debug.Log("Set Evade Player true");
                 }
+                // If player is slower than threshold and enemies are already evading, then stop evading player and set the evation vector anew
                 else if (playerVelocity < playerVelocityTheshold && evadePlayer)
                 {
                     evadePlayer = false;
@@ -120,15 +115,25 @@ namespace PFVR.AI
                     {
                         evadeVector = new Vector3();
                     }
-                    //Debug.Log("Set Evade Player false");
                 }
                 yield return wait;
             }
         }
-        
+
+        // Defines the diffusion vector
+        private void CreateAdditionalDiffusion(float diffusionValue)
+        {
+            randNum = MarsagliaGenerator.Next();
+            diffusionVector.x += diffusionValue * randNum;
+            randNum = MarsagliaGenerator.Next();
+            diffusionVector.y += diffusionValue * randNum;
+            randNum = MarsagliaGenerator.Next();
+            diffusionVector.z += diffusionValue * randNum;
+        }
+
         // Update is called once per frame
         // Guidance System for agents is implemented here
-        void Update()
+        void FixedUpdate()
         {
             if (!nearestGoal) {
                 return;
@@ -137,17 +142,13 @@ namespace PFVR.AI
             // Set nearest goal
             transformationVector = new Vector3(nearestGoal.position.x, nearestGoal.position.y, nearestGoal.position.z) - transform.position;
             alphaFactorUsed = alphaFactor;
-
-            // *** Create Random Factor for diffusion ***
-            randomVector = MarsagliaGenerator.NextVector3();
-
+            
             // *** Evade Player ***
             // Todo: Get it working! Aktuell kummuliert hier irgendwas, sodass es immer mehr lagged, je öfter der player durchfliegt!
             if(evadePlayer)
             {
                 Debug.Log("Evade the Player detected!");
                 //Find out the direction from which player is coming and evade to the sides!
-                evadeBehavior = EnemyEvation.FindEvadeBehavior(evadeDirection);
                 if (evadeVector.magnitude <= 0.0f)
                 {
                     evadeVector = evadeBehavior(playerMotor.position, transform.position);
@@ -156,37 +157,28 @@ namespace PFVR.AI
                 alphaFactorUsed = alphaFactor + 30.0f;
             }
 
-
-            // *** Put together the parts ***
+            // *** Create diffusion ***
+            randomVector = MarsagliaGenerator.NextVector3();
             // Diffusion vector: position of goal + position of diffusion vector minus the position of the agent
             diffusionVector = transformationVector + randomVector * Vector3.Distance(nearestGoal.position, transform.position) * betaFactor;
-            //diffusionVector = new Vector3(nearestGoal.position.x + diffusionX, nearestGoal.position.y + diffusionY, nearestGoal.position.z + diffusionZ) - transform.position;
             
             // Additional chasing behavior
             // Diffuse more when not chasing player
             if (!leaderBehavior.chasePlayer)
             {
                 // Add more diffusion when idle
-                randNum = MarsagliaGenerator.Next();
-                diffusionVector.x += idleDiffusion * randNum;
-                randNum = MarsagliaGenerator.Next();
-                diffusionVector.y += idleDiffusion * randNum;
-                randNum = MarsagliaGenerator.Next();
-                diffusionVector.z += idleDiffusion * randNum;
+                CreateAdditionalDiffusion(idleDiffusion);
             }
             else
             {
                 // Concentrate / Diffuse less when chasing player
-                randNum = MarsagliaGenerator.Next();
-                diffusionVector.x += chasingDiffusion * randNum;
-                randNum = MarsagliaGenerator.Next();
-                diffusionVector.y += chasingDiffusion * randNum;
-                randNum = MarsagliaGenerator.Next();
-                diffusionVector.z += chasingDiffusion * randNum;
+                CreateAdditionalDiffusion(chasingDiffusion);
             }
-            
+
+            // *** Put together the parts ***
             finalMovementVector = transformationVector * alphaFactorUsed + diffusionVector;
 
+            // *** Move the object ***
             //thisRigidbody.AddRelativeForce(finalMovementVector);
             transform.Translate(finalMovementVector * Time.deltaTime);
         }
