017645e60163ecb9f0e75bb16c179faaa6e83ff2
Katharina Broswik <Katharina_B@live.de>
2019-08-14 03:10:30 +0200

    Fixed enemy death

--- Assets/Scripts/OurPhysics/AnimatedDestroyable.cs
+++ Assets/Scripts/OurPhysics/AnimatedDestroyable.cs
@@ -8,6 +8,7 @@ namespace PFVR.OurPhysics {
     public class AnimatedDestroyable : MonoBehaviour, IDestroyable {
         [SerializeField, Range(1, 100)]
         private float maxHP = 1;
+        private float waited = 0.0f;
         public float currentHP {
             get => currentHPCache;
             set {
@@ -37,9 +38,14 @@ namespace PFVR.OurPhysics {
         void Update() {
             if (isAlive && currentHP <= 0) {
                 isAlive = false;
-                animator.SetTrigger("Dead");
-                Destroy(gameObject);
+                animator.SetBool("IsAlive", false);
+                waited = 0.0f;
             }
+            //if(!isAlive && animator.GetAnimatorTransitionInfo(1).duration <= waited)
+            //{
+            //    Destroy(gameObject);
+            //}
+            //waited += Time.deltaTime;
         }
     }
 }
