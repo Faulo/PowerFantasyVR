5557290a120ee52f546ce0b676ad5a37c5f0509e
Katharina Broswik <katharina_B@live.de>
2019-08-14 10:58:21 +0200

    Paul!

--- Assets/Scripts/Player/PowerUp.cs
+++ Assets/Scripts/Player/PowerUp.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using PFVR.ScriptableObjects;
+using PFVR.Events;
 using Slothsoft.UnityExtensions;
 
 namespace PFVR.Player {
+    [RequireComponent(typeof(GameEventSource))]
     public class PowerUp : MonoBehaviour {
         [SerializeField]
         private Gesture gestureToUnlock = default;
         [SerializeField]
         private MeshRenderer[] meshesToColorize = default;
 
+        private GameEventSource eventTarget;
+
         // Start is called before the first frame update
         void Start() {
+            eventTarget = GetComponent<GameEventSource>();
             if (gestureToUnlock) {
                 meshesToColorize.ForAll(meshRenderer => meshRenderer.material.SetColor("_EmissionColor", gestureToUnlock.spellColor));
             }
@@ -22,6 +27,7 @@ namespace PFVR.Player {
             var connector = collider.gameObject.GetComponentInParent<GestureConnector>();
             if (connector) {
                 connector.Unlock(gestureToUnlock);
+                eventTarget.Raise(GameEventType.PowerUpCollected);
                 Destroy(gameObject);
             }
         }
