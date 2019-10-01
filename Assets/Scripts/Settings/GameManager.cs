e31093d36186cb9f10bb965a82b98f6629f26fad
Daniel Schulz <faulolio@gmail.com>
2019-10-01 18:55:01 +0200

    put screenshot function in GameManager and special keys in GameSettings

--- Assets/Scripts/Settings/GameManager.cs
+++ Assets/Scripts/Settings/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using PFVR.Player;
+﻿using System;
+using PFVR.Player;
 using PFVR.ScriptableObjects;
 using TMPro;
 using UnityEditor;
@@ -6,6 +7,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace PFVR.Settings {
+    /// <summary>
+    /// Some global keybindings and level load code.
+    /// </summary>
     public class GameManager : MonoBehaviour {
         public static GameManager instance {
             get {
@@ -20,15 +24,34 @@ namespace PFVR.Settings {
         public GameSettings settings;
 
         void Update() {
-            if (Input.GetKeyDown(KeyCode.Escape)) {
-                if (SceneManager.GetActiveScene().path == settings.mainMenu.ScenePath) {
-                    Application.Quit();
+            if (Input.GetKeyDown(settings.menuKey)) {
+                if (IsInMainMenu()) {
+                    Exit();
                 } else {
-                    LoadScene(settings.mainMenu);
+                    LoadMainMenu();
                 }
             }
+            if (Input.GetKeyDown(settings.screenshotKey)) {
+                TakeScreenshot();
+            }
         }
+
+        private bool IsInMainMenu() {
+            return SceneManager.GetActiveScene().path == settings.mainMenu.ScenePath;
+        }
+
+        public void Exit() {
+            Application.Quit();
+        }
+
+        public void TakeScreenshot() {
+            var name = string.Format(settings.screenshotPath, DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss"));
+            ScreenCapture.CaptureScreenshot(name, 4);
+            Debug.Log("Screenshot '" + name + "' done!");
+        }
+
         private void LoadScene(SceneReference scene) => SceneManager.LoadScene(scene.ScenePath);
         public void LoadLevel(Level level) => LoadScene(level.scene);
+        public void LoadMainMenu() => LoadScene(settings.mainMenu);
     }
 }
